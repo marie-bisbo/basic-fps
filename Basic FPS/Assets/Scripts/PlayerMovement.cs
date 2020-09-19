c07f9454d6248da05d69cebc54e2054e42895677
marie-bisbo <marie@brainlabsdigital.com>
2020-09-19 14:56:21 +0100

    Add gravity to player

--- Basic FPS/Assets/Scripts/PlayerMovement.cs
+++ Basic FPS/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,14 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController characterController;
 
     public float movementSpeed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +24,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = transform.right * xInput + transform.forward * zInput;
 
         characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
