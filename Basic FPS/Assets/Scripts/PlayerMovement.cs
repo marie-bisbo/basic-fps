ff65127b7c321e6dbb0307374fa1791478945b30
marie-bisbo <marie@brainlabsdigital.com>
2020-09-19 14:45:54 +0100

    Rename variables and move scripts to separate folder

---
+++ Basic FPS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+
+    public float movementSpeed = 12f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = transform.right * xInput + transform.forward * zInput;
+
+        characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+    }
+}
