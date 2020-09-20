46c5a9452ac879f992c599af8ee97499ac5dc666
marie-bisbo <marie@brainlabsdigital.com>
2020-09-20 09:42:10 +0100

    Add teleportation grenade with debug statement on detonation

---
+++ Basic FPS/Assets/TeleportationGrenade.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class TeleportationGrenade : MonoBehaviour
+{
+
+    public float detonationDelay = 3f;
+
+    float countdown;
+    bool hasDetonated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = detonationDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown();
+        IsDetonating();
+    }
+
+    void CountDown()
+    {
+        countdown -= Time.deltaTime;
+    }
+
+    void IsDetonating()
+    {
+        if (countdown <= 0f && !hasDetonated)
+        {
+            Detonate();
+            hasDetonated = true;
+        }
+    }
+
+    void Detonate()
+    {
+        Debug.Log("TELEPORT");
+    }
+}
