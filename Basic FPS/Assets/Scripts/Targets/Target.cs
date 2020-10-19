c6fdd11b47c7f4c19ed135ff8603c6d1feac6869
marie-bisbo <marie@brainlabsdigital.com>
2020-10-19 17:39:24 +0100

    Multiply cube on hit, expand on death

--- Basic FPS/Assets/Scripts/Targets/Target.cs
+++ Basic FPS/Assets/Scripts/Targets/Target.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Target : MonoBehaviour
 {
@@ -11,10 +12,40 @@ public class Target : MonoBehaviour
     public void TakeDamage (float amount)
     {
         health -= amount;
-        if (health <= 0)
+        if (health <= 0 && transform.localScale.x > 1f)
         {
-            Die();
+            Multiply();
         }
+        else
+        {
+            StartCoroutine(ScaleOverTime(2));
+        }
+    }
+
+    void Multiply ()
+    {
+        GameObject cube1 = Instantiate(gameObject, transform.position + new Vector3(5f, 0f), transform.rotation);
+        GameObject cube2 = Instantiate(gameObject, transform.position + new Vector3(-5f, 0f), transform.rotation);
+        cube1.transform.localScale *= 0.5f;
+        cube2.transform.localScale *= 0.5f;
+        Destroy(gameObject);
+    }
+
+    IEnumerator ScaleOverTime(float time)
+    {
+        Vector3 originalScale = gameObject.transform.localScale;
+        Vector3 destinationScale = new Vector3(2.0f, 2.0f, 2.0f);
+
+        float currentTime = 0.0f;
+
+        do
+        {
+            gameObject.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+
+        Die();
     }
 
     void Die ()
@@ -23,7 +54,7 @@ public class Target : MonoBehaviour
         Rigidbody[] allRigidBodies = destroyedObject.GetComponentsInChildren<Rigidbody>();
         if (allRigidBodies.Length > 0)
         {
-            foreach(var body in allRigidBodies)
+            foreach (var body in allRigidBodies)
             {
                 body.AddExplosionForce(explosionForce, transform.position, 1);
             }
